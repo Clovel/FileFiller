180e16797e168469a40ccef35b5b85458291e95a
Clovis Durand <cd.clovel19@gmail.com>
2020-04-14 20:28:57 +0200

    Updated FileFiller-util main.cpp
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 /* Includes -------------------------------------------- */
 #include "FileFiller.hpp"
+#include "FileFillerTagFactory.hpp"
 
 /* C++ system */
 #include <iostream>
@@ -21,6 +22,7 @@ static void printUsage(const char * const pProgName)
     std::cout << "[USAGE] " << pProgName << std::endl;
     std::cout << "        <arg1> - Input file path" << std::endl;
     std::cout << "        <arg2> - Output file path" << std::endl;
+    std::cout << "        <arg3> - Tag mapping file path" << std::endl;
 }
 
 /* ----------------------------------------------------- */
@@ -28,26 +30,30 @@ static void printUsage(const char * const pProgName)
 /* ----------------------------------------------------- */
 int main(const int argc, const char * const * const argv) {
     /* Check arguments */
-    if ((3 > argc) || (0 == strcmp(argv[1U], "--help"))) {
+    if ((4 > argc) || (0 == strcmp(argv[1U], "--help"))) {
         printUsage(argv[0]);
         return EXIT_FAILURE;
     }
 
     const std::string lInputFilePath(argv[1U]);
     const std::string lOutputFilePath(argv[2U]);
+    const std::string lTagFilePath(argv[3U]);
 
     /* Build a replacement map */
-
-    /* Get the input string from the input file */
-
-    /* Create a file filler */
-    FileFiller lParser;
+    std::map<std::string, std::string> lMap;
+    int lResult = FileFillerTagFactory::parseTagMapFile(lTagFilePath, lMap);
+    if(0 > lResult) {
+        std::cerr << "[ERROR] Failed to parse tag mapping file" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     /* Parse the input and procede to replace the tags */
+    std::string lOutput = "";
 
-    /* Get the output string */
+    /* Create a file filler */
+    FileFiller::parseFile(lMap, lInputFilePath, lOutputFilePath, lOutput);
 
-    /* Create the output file */
+    /* TODO : If verbose, print output */
 
     return EXIT_SUCCESS;
 }
