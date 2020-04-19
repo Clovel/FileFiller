970c8d6a07b884be2261f5d5504ea17611f208b2
Clovis Durand <cd.clovel19@gmail.com>
2020-04-19 17:34:25 +0200

    [#1] Factored file reading in FileFiller.cpp using readFile method
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/FileFiller.cpp
+++ src/FileFiller.cpp
@@ -264,43 +264,10 @@ int FileFiller::parseString(std::string * const pOut) {
 int FileFiller::parseFile(std::string * const pOut) {
     int lReplacements = 0;
 
-    /* Try to open the file */
-    std::ifstream lIFS(mInputFilePath.c_str(), std::ios::in);
-    if(!lIFS) {
-        std::cerr << "[ERROR] <FileFiller::parseFile> Failed to open the input file : " << mInputFilePath << std::endl;
-        return -1;
+    /* Read input file */
+    if(0 > readFile()) {
+        std::cerr << "[ERROR] <FileFiller::parseFile> readFile failed" << std::endl;
     }
-
-    /* Get lenght of the file */
-    lIFS.seekg(0, lIFS.end);
-    const int lFileLength = lIFS.tellg();
-
-    /* Reset the position of the next character to the beginning of the file */
-    lIFS.seekg(0, lIFS.beg);
-
-    /* Read the whole file and set the contents as our input string */
-    {
-        char *lBuf = (char *)malloc(lFileLength);
-        lIFS.read(lBuf, lFileLength);
-        if(!lIFS) {
-            std::cerr << "[ERROR] <FileFiller::parseFile> Error occured while reading file contents" << std::endl;
-
-            /* Free the allocated memory */
-            free(lBuf);
-
-            /* Close the input file */
-            lIFS.close();
-
-            return -1;
-        }
-        mInputStr = std::string(lBuf);
-
-        /* Free the allocated memory */
-        free(lBuf);
-    }
-
-    /* Close the input file */
-    lIFS.close();
 
     /* Parse the input string */
     lReplacements = parseString(pOut);
